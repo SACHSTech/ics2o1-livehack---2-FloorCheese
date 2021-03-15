bc10bb8220a390a97874a5b8c2eb59e2dfc17877
Eric Fabroa <eric.fabroa@ycdsbk12.ca>
2021-03-15 01:41:46 +0000

    submit

--- problem2.py
+++ problem2.py
@@ -1,1 +1,8 @@
+first_side = float(input("Enter first side: "))
+second_side = float(input("Enter second side: "))
+third_side = float(input("Enter third side: "))
 
+if first_side + second_side > third_side or second_side + third_side > first_side or first_side + third_side > second_side:
+  print ("The figure is a triangle")
+else: 
+  print ("The figure is NOT a triangle")
